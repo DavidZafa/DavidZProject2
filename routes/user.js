ba26270922ac914e9d68535adaccd97f3feee675
DavidZ <david.zafirovic@gmail.com>
2018-08-02 12:42:04 -0400

    working on data

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user.js');
+const userController = require('../controllers/user');
+
 
 router.get('/login', userController.login);
 router.post('/login', userController.createLogin);
 router.get('/sign-up', userController.signUp);
 router.post('/sign-up', userController.createSignUp);
-router.get('/profile', userController.profile)
 router.get('/logout', userController.logout);
 router.get('/:id', userController.show);
-router.put('/:id', userController.update)
+
+
 module.exports = router;
